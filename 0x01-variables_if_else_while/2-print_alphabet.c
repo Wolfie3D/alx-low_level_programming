8ae9af725cbeffe9e52ab5fc802cc3937c3fc56f
Wolfie3D <kwangechi3@gmail.com>
2023-02-17 23:03:15 +0300

    adding 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints the alphabet in lowercase.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
