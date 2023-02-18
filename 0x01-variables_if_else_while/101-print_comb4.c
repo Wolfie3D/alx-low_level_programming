6acd4ab1db11131e1dc91ac66e929d2635497d4a
Wolfie3D <kwangechi3@gmail.com>
2023-02-18 05:12:34 +0300

    adding 101-print_comb5.c fie

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,29 +2,31 @@
 
 /**
  * main - Prints all possible combinations of two two-digit numbers,
- *ranging from 0-99, separated by a comma followed by a space.
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int num1, num2;
-	char separator[] = ", ";
+    int num1, num2;
+    int comma = 44;  // ASCII code for comma character
+    int space = 32;  // ASCII code for space character
 
-	for (num1 = 0; num1 <= 98; num1++)
-	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			printf("%s", separator);
-		}
-	}
+    for (num1 = 0; num1 <= 98; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
+            putchar((num1 / 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar(space);
+            putchar((num2 / 10) + '0');
+            putchar((num2 % 10) + '0');
+            putchar(comma);
+            putchar(space);
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
