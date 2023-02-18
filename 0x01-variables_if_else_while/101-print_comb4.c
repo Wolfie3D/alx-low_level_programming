a8754700900211511da8c77f5869c498621ccb22
Wolfie3D <kwangechi3@gmail.com>
2023-02-18 04:58:57 +0300

    adding 101-print_comb5.c fie

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
+nclude <stdio.h>
 
-/**
- * main - Prints all possible combinations of two two-digit numbers, 
- * ranging from 0-99, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	int num1, num2;
+    int num1, num2;
+    char output[600] = ""; // allocate a character array to hold the output string
 
-	for (num1 = 0; num1 <= 98; num1++)
-	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+    for (num1 = 0; num1 <= 98; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
+            // build the output string by appending each digit and separator
+            output[strlen(output)] = (num1 / 10) + '0';
+            output[strlen(output)] = (num1 % 10) + '0';
+            output[strlen(output)] = ' ';
+            output[strlen(output)] = (num2 / 10) + '0';
+            output[strlen(output)] = (num2 % 10) + '0';
 
-			if (num1 == 98 && num2 == 99)
-				continue;
+            if (num1 == 98 && num2 == 99) {
+                // do not add a comma after the last pair of numbers
+            } else {
+                output[strlen(output)] = ',';
+                output[strlen(output)] = ' ';
+            }
+        }
+    }
 
-			putchar(',');
-		}
-	}
+    printf("%s\n", output); // print the whole output string using a single printf call
 
-	putchar('\n');
-
-	return (0);
+    return 0;
 }
