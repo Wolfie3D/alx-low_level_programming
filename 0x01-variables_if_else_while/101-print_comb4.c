1c7afcc0917f1774b8a23a0f9c4b070f6fd9db5d
Wolfie3D <kwangechi3@gmail.com>
2023-02-18 05:26:19 +0300

    adding 101-print_comb5.c fie

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three digits in ascending order.
- *
- * Return: Always 0.
- */
+* main - Prints all possible combinations of three digits in ascending order.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                putchar((i % 10) + '0');
-                putchar((j % 10) + '0');
-                putchar((k % 10) + '0');
+for (i = 0; i <= 7; i++)
+{
+for (j = i + 1; j <= 8; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar((k % 10) + '0');
 
-                if (i == 7 && j == 8 && k == 9)
-                    continue;
+if (i == 7 && j == 8 && k == 9)
+continue;
 
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
