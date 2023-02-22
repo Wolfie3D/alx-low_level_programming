431a6c4f749412f72cb7490cc923d638001143a9
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 06:21:54 +0300

    1-alphabet.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
-#include <unistd.h>
-/**
- * main - prints "_putchar", followed by a new line
- *
- * Return: Always 0
- */
+#include "main.h"
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+    char str[] = "_putchar\n";
+
+    for (int i = 0; i < 9; i++)
+        _putchar(str[i]);
 
     return (0);
 }
