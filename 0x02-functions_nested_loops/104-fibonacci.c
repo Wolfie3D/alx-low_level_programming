aaf3ca3f1c99f1e8d06668f3a6616300df12f94c
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 22:42:52 +0300

    adding 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers
- * Return: Always 0
- */
+* main - prints the first 98 Fibonacci numbers
+* Return: Always 0
+*/
 
 int main(void)
 
