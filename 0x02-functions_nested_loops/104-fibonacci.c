5627417d4530b1bbd41b2c6082e24a8e4db61d79
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 23:05:09 +0300

    adding 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,8 @@ long long int first = 1, second = 2, next;
 
 printf("%lld, %lld, ", first, second);
 
-for (i = 3; i <= 98; i++) {
+for (i = 3; i <= 98; i++) 
+{
 next = first + second;
 printf("%lld", next);
 
@@ -28,5 +29,5 @@ second = next;
 
 printf("\n");
 
-return 0;
+return (0);
 }
