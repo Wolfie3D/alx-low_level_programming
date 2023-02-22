001d1e1b535ae7625fbd872f99f327e466641313
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 21:02:43 +0300

    adding 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- *
- * @num: number to check sign of
- *
- * Return: 1 and prints + if num is greater than zero,
- *         0 and prints 0 if num is zero,
- *         -1 and prints - if num is less than zero
- */
+* print_sign - prints the sign of a number
+*
+* @num: number to check sign of
+*
+* Return: 1 and prints + if num is greater than zero,
+*         0 and prints 0 if num is zero,
+*         -1 and prints - if num is less than zero
+*/
 int print_sign(int num)
 {
-    int result;
+int result;
 
-    if (num > 0)
-    {
-        _putchar('+');
-        result = 1;
-    }
-    else if (num == 0)
-    {
-        _putchar('0');
-        result = 0;
-    }
-    else
-    {
-        _putchar('-');
-        result = -1;
-    }
+if (num > 0)
+{
+_putchar('+');
+result = 1;
+}
+else if (num == 0)
+{
+_putchar('0');
+result = 0;
+}
+else
+{
+_putchar('-');
+result = -1;
+}
 
-    return (result);
+return (result);
 }	 
