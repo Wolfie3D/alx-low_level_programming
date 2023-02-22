ef0f8e2588779309c8af62a0c6c11ab018f5be06
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 22:09:47 +0300

    adding 100-times_table.c file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_times_table - prints the n times table, starting with 0.
+* @n: the multiplication table to print
+*
+* Return: void
+*/
+void print_times_table(int n)
+{
+int i, j, res;
+
+if (n < 0 || n > 15)
+return;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+res = i * j;
+if (j == 0)
+printf("%d", res);
+else if (res < 10)
+printf(",   %d", res);
+else if (res < 100)
+printf(",  %d", res);
+else
+printf(", %d", res);
+}
+printf("\n");
+}
+}
