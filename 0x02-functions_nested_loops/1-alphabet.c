5021eb452fbed463f63b38a65630718bd0067406
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 20:36:27 +0300

    added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * 
- *
- * print_alphabet - prints the lowercase alphabet
- */
+* print_alphabet - Prints the alphabet in lowercase.
+*
+* Description:
+*   This function prints the lowercase alphabet to the console, followed
+*   by a newline character '\n'. It uses two _putchar statements to print
+*   the letters of the alphabet one by one.
+*
+* Return: void.
+*/
 void print_alphabet(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
 }
