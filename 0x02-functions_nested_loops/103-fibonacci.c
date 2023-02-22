3dfd7c2ce2f7db1f06d76abcbac49f56edda9dfc
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 22:26:05 +0300

    adding 103-fibonacci.c file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+long int sum = 0, a = 1, b = 2, c;
+
+while (b <= 4000000)
+{
+if (b % 2 == 0)
+sum += b;
+
+c = a + b;
+a = b;
+b = c;
+}
+
+printf("%ld\n", sum);
+
+return 0;
+}
