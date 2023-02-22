5e04e6522ab4acdca84999ddf1418abcd566631d
Wolfie3D <kwangechi3@gmail.com>
2023-02-22 21:57:49 +0300

    adding 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,34 @@
-_table - Prints the 9 times table, starting with 0
-*
-* Return: void
-*/
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: no return value
+ */
 void times_table(void)
 {
-int i, j, result;
+    int i, j, result;
 
-for (i = 0; i <= 10; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-result = i * j;
-
-if (j == 0)
-_putchar(result + '0');
-else if (result <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(result + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(result / 10 + '0');
-_putchar(result % 10 + '0');
-}
-}
-_putchar('\n');
-}
+    for (i = 0; i < 10; i++)
+    {
+        _putchar('0');
+        for (j = 1; j < 10; j++)
+        {
+            _putchar(',');
+            _putchar(' ');
+            result = i * j;
+            if (result < 10)
+            {
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
