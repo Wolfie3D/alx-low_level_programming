a00e0bf11dd72df71d1366aa6649fd10736de40a
Wolfie3D <kwangechi3@gmail.com>
2023-02-23 19:28:39 +0300

    added 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include < stdio.h >
-
+#include <unistd.h>
 /**
-* main - check the code for Holberton School students.
+* _putchar - writes the character c to stdout
+* @c: The character to print
 *
-* Return: Always 0.
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
-int main(void)
+int _putchar(char c)
 {
-char c;
-
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
+return (write(1, &c, 1));
 }
