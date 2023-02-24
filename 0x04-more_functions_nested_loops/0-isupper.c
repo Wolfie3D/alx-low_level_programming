0a0315dd7917c029ffab48460fa666196c9640fd
Wolfie3D <kwangechi3@gmail.com>
2023-02-24 08:50:13 +0300

    task 0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
 {
-return (write(1, &c, 1));
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
